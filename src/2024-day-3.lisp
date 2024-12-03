89878373abca65441e26ce2a587716cf58d08971
Edward Steere <edward.steere@gmail.com>
2024-12-03 07:15:44 +0200

    Solve day 3

--- src/2024-day-3.lisp
+++ src/2024-day-3.lisp
@@ -14,4 +14,29 @@
       (recur))))
 
 (defun part-1 ()
-  )
+  (iter
+    (for line in (read-problem))
+    (summing
+     (iter
+       (for mul in (cl-ppcre:all-matches-as-strings "mul\\((\\d+),(\\d+)\\)" line))
+       (summing (match mul
+                  ((ppcre "mul\\((\\d+),(\\d+)\\)"
+                          (read a )
+                          (read b))
+                   (* a b))))))))
+
+(defun part-2 ()
+  (iter outer
+    (for line in (read-problem))
+    (with enabled = t)
+    (iter
+      (for mul in (cl-ppcre:all-matches-as-strings "(mul\\((\\d+),(\\d+)\\))|(don't\\(\\))|(do\\(\\))" line))
+      (match mul
+        ((ppcre "mul\\((\\d+),(\\d+)\\)"
+                (read a)
+                (read b))
+         (when enabled (in outer (summing (* a b)))))
+        ((ppcre "don't\\(\\)")
+         (setf enabled nil))
+        ((ppcre "do\\(\\)")
+         (setf enabled t))))))
