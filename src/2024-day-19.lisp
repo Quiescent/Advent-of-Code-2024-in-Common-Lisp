a689ee21537bcaf138dcbc6beec4fab450c21fea
Edward Steere <edward.steere@gmail.com>
2024-12-18 07:31:33 +0200

    Add a placeholder for day 19

---
+++ src/2024-day-19.lisp
@@ -0,0 +1,17 @@
+(defpackage 2024-day-19
+  (:use :cl :iterate :cl-ppcre :metabang-bind :trivia :trivia.ppcre)
+  (:shadowing-import-from :arrow-macros :->>))
+(in-package 2024-day-19)
+(neat-lambda:enable-lambda-syntax)
+(currying:enable-currying-syntax)
+
+(defun read-problem ()
+  (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2024 "src/2024-day-19.in"))
+    (labels ((recur ()
+               (bind ((line (read-line f nil nil)))
+                 (when line
+                   (cons line (recur))))))
+      (recur))))
+
+(defun part-1 ()
+  )
